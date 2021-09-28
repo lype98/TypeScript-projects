9e912acdcfde04f98c6412b8341535c1568b8d58
ultimatelype <ultimatelype98@gmail.com>
2021-09-28 13:39:57 +0100

    Update index.js

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,6 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([444, 6, -8, 0.12, 45, 77]);
+const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedList_1 = require("./LinkedList");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([444, 6, -8, 0, 45, 77]);
 numbersCollection.sort();
-console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('zyxwvutsrqponmlkjlhgfedcba');
+charactersCollection.sort();
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(455);
+linkedList.add(78);
+linkedList.add(3);
+linkedList.add(-4);
+console.log(`Numbers sort: ${numbersCollection.data}
+Characters sort: ${charactersCollection.data}`);
+linkedList.print();
