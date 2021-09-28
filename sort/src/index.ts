cc5c1d0ae2d6e7a72bb22d858888089fb8434109
ultimatelype <ultimatelype98@gmail.com>
2021-09-28 13:39:35 +0100

    Update index.ts

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,21 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([444, 6, -8, 0.12, 45, 77]);
+const numbersCollection = new NumbersCollection([444, 6, -8, 0, 45, 77]);
 numbersCollection.sort();
-console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection(
+  'zyxwvutsrqponmlkjlhgfedcba'
+);
+charactersCollection.sort();
+
+const linkedList = new LinkedList();
+linkedList.add(455);
+linkedList.add(78);
+linkedList.add(3);
+linkedList.add(-4);
+
+console.log(`Numbers sort: ${numbersCollection.data}
+Characters sort: ${charactersCollection.data}`);
+linkedList.print();
